ab30b92c60bacb9a118845a1b7ce304d98027ae6
Davi Lopes <davilopesrdgs@gmail.com>
2025-08-31 17:22:14 -0300

    feat: desenv. modulo expense

---
+++ src/modules/expense/expense.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ExpenseController } from './expense.controller';
+import { ExpenseService } from './expense.service';
+
+@Module({
+  imports: [],
+  controllers: [ExpenseController],
+  providers: [ExpenseService],
+  exports: [],
+})
+export class ExpenseModule {}
