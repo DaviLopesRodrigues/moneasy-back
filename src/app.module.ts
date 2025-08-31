bf261073629147471572926d826ba83195e87ff3
Davi Lopes <davilopesrdgs@gmail.com>
2025-08-31 20:06:42 -0300

    feat: instal, config e uso prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IncomeModule } from './modules/income/income.module';
 import { ExpenseModule } from './modules/expense/expense.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [IncomeModule, ExpenseModule],
+  imports: [IncomeModule, ExpenseModule, PrismaModule],
   controllers: [],
   providers: [],
 })
